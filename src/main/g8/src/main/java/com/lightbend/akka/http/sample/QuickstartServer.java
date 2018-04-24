744f50151b2a142109899a30af14111aeb59bbce
Arnout Engelen <arnouten@bzzt.net>
2018-04-24 14:46:58 +0200

    Remove unused imports

--- src/main/g8/src/main/java/com/lightbend/akka/http/sample/QuickstartServer.java
+++ src/main/g8/src/main/java/com/lightbend/akka/http/sample/QuickstartServer.java
@@ -5,15 +5,12 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
-import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
-
-import java.util.concurrent.CompletionStage;
 
 //#main-class
 public class QuickstartServer extends AllDirectives {
